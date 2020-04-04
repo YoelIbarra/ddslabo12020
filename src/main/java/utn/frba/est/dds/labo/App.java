f95b58f1297b6cd6c1326912fecc80354fe6dbb3
YoelIbarra <yoelibarra98@gmail.com>
2020-04-04 12:08:27 -0300

    ok

---
+++ src/main/java/utn/frba/est/dds/labo/App.java
@@ -0,0 +1,13 @@
+package utn.frba.est.dds.labo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/utn/frba/est/dds/labo/AppTest.java
@@ -0,0 +1,20 @@
+package utn.frba.est.dds.labo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
